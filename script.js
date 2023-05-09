70d191085f90e0daee8067301fc71a38a280f06f
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-05-09 22:14:17 +0100

    Testimonials

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 10)
 })
+
+
+
+
+// FAQS 
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i')
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"; 
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
